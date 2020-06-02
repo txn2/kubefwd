6fd81ccef67a07507a32ad228ecbc407034905ff
Mathieu Hinderyckx <mathieu.hinderyckx@gmail.com>
2020-06-02 20:12:36 +0200

    Threadsafety change

--- cmd/kubefwd/services/services.go
+++ cmd/kubefwd/services/services.go
@@ -230,6 +230,8 @@ Try:
 		contexts = append(contexts, rawConfig.CurrentContext)
 	}
 
+	hostFileWLock := &fwdport.HostFileWithLock{Hosts: hostFile}
+
 	for i, ctx := range contexts {
 		// k8s REST config
 		restConfig, err := configGetter.GetRestConfig(cfgFilePath, ctx)
@@ -264,7 +266,7 @@ Try:
 				Context:      ctx,
 				Namespace:    namespace,
 				ListOptions:  listOptions,
-				Hostfile:     &fwdport.HostFileWithLock{Hosts: hostFile},
+				Hostfile:     hostFileWLock,
 				ClientConfig: restConfig,
 				RESTClient:   restClient,
 				ShortName:    i < 1 && ii < 1,
